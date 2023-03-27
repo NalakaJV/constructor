3b135e1e8cf5e5e992356835ebe6a7978451e6f0
NalakaJV <nalakamadushan430@gmail.com>
2023-03-27 21:50:02 +0530

    ConstructorJava Project Code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    Main() {
+        System.out.println("java");
+        String name = "Programming Language";
+    }
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+}
---
+++ src/defaultvalue.java
@@ -0,0 +1,9 @@
+public class defaultvalue {
+    int x;
+    boolean y;
+    public static void main(String[] args) {
+        defaultvalue dv = new defaultvalue();
+        System.out.println(dv.x);
+        System.out.println(dv.y);
+    }
+}
